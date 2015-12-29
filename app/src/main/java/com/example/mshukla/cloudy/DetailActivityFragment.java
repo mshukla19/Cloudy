c434b3ec5355867b7869e753b0df3caba41c9e84
Manas Shukla <mnsshukla94@gmail.com>
2015-12-29 20:22:46 +0530

    Share Intent completed (Lesson 3)

--- app/src/main/java/com/example/mshukla/cloudy/DetailActivityFragment.java
+++ app/src/main/java/com/example/mshukla/cloudy/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.example.mshukla.cloudy;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -18,7 +24,28 @@ public class DetailActivityFragment extends Fragment {
 
     @Bind(R.id.detail_item)
     TextView weatherDetails;
+
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_STRING = " #SUNSHINE";
+    private String mForecastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if(mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createForecastShareIntent());
+        } else {
+            Log.d(LOG_TAG,"Share Action Provider is null");
+        }
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
@@ -28,8 +55,18 @@ public class DetailActivityFragment extends Fragment {
         ButterKnife.bind(this,rootView);
         Intent intent = getActivity().getIntent();
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            weatherDetails.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            weatherDetails.setText(mForecastStr);
         }
         return rootView;
     }
+
+    private Intent createForecastShareIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,mForecastStr + FORECAST_STRING);
+        return intent;
+    }
+
 }
