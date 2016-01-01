3e850bc326eacf193658febbe5c64e963055abfa
Manas Shukla <mnsshukla94@gmail.com>
2016-01-01 18:59:34 +0530

    Content Provider added
    Details View broken

--- app/src/main/java/com/example/mshukla/cloudy/MainActivity.java
+++ app/src/main/java/com/example/mshukla/cloudy/MainActivity.java
@@ -10,9 +10,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,28 +56,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (id == R.id.action_map) {
-            openPreferedLocationinMap();
+            openPreferredLocationInMap();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void openPreferedLocationinMap() {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String location = preferences.getString
-                (getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+    private void openPreferredLocationInMap() {
+        String location = Utility.getPreferredLocation(this);
 
+        // Using the URI scheme for showing a location found on a map.  This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/components/intents-common.html#Maps
         Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
-                .appendQueryParameter("q",location)
+                .appendQueryParameter("q", location)
                 .build();
+
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(geoLocation);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
-        }
-        else {
-            Log.d(LOG_TAG, "Couldn't call" + location + ", no compatible app exist");
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
         }
     }
 }
